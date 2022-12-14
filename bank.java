64ba8b9c91d632f1449202e8aa7a49f6202c58fc
Marie Cari <mcari@nyc.yearup.org>
2022-12-14 18:22:47 -0500

    declared variables

--- bank.java
+++ bank.java
@@ -1,4 +1,10 @@
 public class bank {
+    //declare variables
+    private String firstName;
+    private String lastName;
+    private int balance;
+    private int lastTransactionAmount;
+
     //create constructor for first and last name
 
     //check balance
