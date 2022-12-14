3c88d039f09088fea6a594ca7e252f00b48fadeb
Marie Cari <mcari@nyc.yearup.org>
2022-12-14 17:58:30 -0500

    puesdo code

--- bank.java
+++ bank.java
@@ -1,2 +1,16 @@
 public class bank {
+    //create constructor for first and last name
+
+    //check balance
+
+    //make deposit
+
+    //make withdrawal
+
+    //view previous transactions
+
+    //calculate interest
+
+    //exit application
+
 }
